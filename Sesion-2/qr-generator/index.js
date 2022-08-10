ffad522714dadf415da3c0359090b95705b15d09
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-08-10 12:46:15 +0200

    Update qr-generator project in Sesion-2

--- Sesion-2/qr-generator/index.js
+++ Sesion-2/qr-generator/index.js
@@ -1,8 +1,12 @@
+const getQrCodeImageUrl = (url) => `https://qrtag.net/api/qr_5.png?url=${url}`;
+
 const qrContainer = document.querySelector("#qr-container");
+
+const currentUrl = document.location.toString();
 
 // <img src="qr.png" alt="qr code" />
 const qrCode = document.createElement("img");
-qrCode.setAttribute("src", "qr.png");
+qrCode.setAttribute("src", getQrCodeImageUrl(currentUrl));
 qrCode.setAttribute("alt", "qr code");
 
 qrContainer.appendChild(qrCode);
