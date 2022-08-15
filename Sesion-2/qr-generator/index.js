74dfcf5d307e941563b29d62da14ba61713f07a6
Alfredo González Martínez <alfredgonzalezmartinez@gmail.com>
2022-08-15 18:33:02 +0200

    Add form to qr-generator project in Sesion-2

--- Sesion-2/qr-generator/index.js
+++ Sesion-2/qr-generator/index.js
@@ -1,4 +1,16 @@
-const getQrCodeImageUrl = (url) => `https://qrtag.net/api/qr_5.png?url=${url}`;
+const getQrCodeImageUrl = (url) =>
+  `https://qrtag.net/api/qr_transparent_6.png?url=${url}`;
+
+const getAltText = (url) => `qr code for url: ${url}`;
+
+const isValidUrl = (url) => {
+  try {
+    return new URL(url) && true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
 
 const qrContainer = document.querySelector("#qr-container");
 
@@ -7,6 +19,19 @@ const currentUrl = document.location.toString();
 // <img src="qr.png" alt="qr code" />
 const qrCode = document.createElement("img");
 qrCode.setAttribute("src", getQrCodeImageUrl(currentUrl));
-qrCode.setAttribute("alt", "qr code");
+qrCode.setAttribute("alt", getAltText(currentUrl));
 
 qrContainer.appendChild(qrCode);
+
+const urlForm = document.querySelector("#url-form");
+
+urlForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const url = event.target.url.value;
+  if (isValidUrl(url)) {
+    qrCode.setAttribute("src", getQrCodeImageUrl(url));
+    qrCode.setAttribute("alt", getAltText(url));
+  } else {
+    alert("Invalid url");
+  }
+});
